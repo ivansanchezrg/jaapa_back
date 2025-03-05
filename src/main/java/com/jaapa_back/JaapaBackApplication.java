bc4bf82cd2580a11e92a91d5b67baf46733839a1
ivansanchezrg <ivansanchezrg@gmail.com>
2025-03-05 09:05:42 -0500

    Initial commit with Spring Boot project

---
+++ src/main/java/com/jaapa_back/JaapaBackApplication.java
@@ -0,0 +1,13 @@
+package com.jaapa_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaapaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaapaBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaapa_back/JaapaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaapa_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaapaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
